0f113703f4139d92d764d866d82be6f75797386f
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2016-02-14 15:00:18 +0100

    No consigo quitar el problema del javax.faces.Bean. Lo intentaré otra vez desde cero.

--- primefaces/src/main/java/com/alvarogonzalez/autentiaHomework/mappers/ClienteMapper.java
+++ primefaces/src/main/java/com/alvarogonzalez/autentiaHomework/mappers/ClienteMapper.java
@@ -7,18 +7,17 @@ import org.apache.ibatis.annotations.Select;
 import com.alvarogonzalez.autentiaHomework.model.Cliente;
 
 public interface ClienteMapper {
-		  @Insert("INSERT INTO cliente(usuario, password, nombre,"
-		          + "apellidos, fechaNacimiento) VALUES"
-		          + "(#{usuario},#{password}, #{nombre}, #{apellidos},"
-		          + "#{fechaNacimiento})")
-		  @Options(useGeneratedKeys=true, keyProperty="idUsuario", flushCache=true, keyColumn="idUsuario")
-		  public void insertCliente(Cliente student);
+    @Insert("INSERT INTO cliente(usuario, password, nombre,"
+            + "apellidos, fechaNacimiento) VALUES"
+            + "(#{usuario},#{password}, #{nombre}, #{apellidos},"
+            + "#{fechaNacimiento})")
+    @Options(useGeneratedKeys = true, keyProperty = "idUsuario", flushCache = true, keyColumn = "idUsuario")
+    public void insertCliente(Cliente student);
 
-		  @Select("SELECT USUARIO as USUARIO, PASSWORD as PASSWORD, "
-		          + "NOMBRE as NOMBRE, APELLIDOS as APELLIDOS, "
-		          + "FECHANACIMIENTO as FECHANACIMIENTO "
-		          + "FROM clientes WHERE usuario = #{usuario}")
-		  public Cliente getClienteByUsuario(String usuario);
+    @Select("SELECT USUARIO as USUARIO, PASSWORD as PASSWORD, "
+            + "NOMBRE as NOMBRE, APELLIDOS as APELLIDOS, "
+            + "FECHANACIMIENTO as FECHANACIMIENTO "
+            + "FROM clientes WHERE usuario = #{usuario}")
+    public Cliente getClienteByUsuario(String usuario);
 
-	
 }
--- readme.txt
+++ readme.txt
@@ -1,8 +1,12 @@
 
-PASO 1
-Poner proxy a maven
+
+PROXY EN MAVEN
+
+CREACION DE PROYECTO
 mvn archetype:generate -DgroupId=com.alvarogonzalez.autentiaHomework -DartifactId=primefaces -DarchetypeArtifactId=maven-archetype-webapp
 
-PASO 2: dependencias
+DEPENDENCIAS
 http://www.javacodegeeks.com/2014/02/building-java-web-application-using-mybatis-with-spring.html
+
+
 
